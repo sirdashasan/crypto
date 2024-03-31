22a14c0e7c17a48f520d1e994a9d24aa89b7f89b
sirdashasan <sirdashasan@hotmail.com>
2024-03-31 21:36:11 +0300

    trending coins is completed

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,5 +1,18 @@
+import useAxios from "../hooks/useAxios";
+import CoinTrending from "./CoinTrending";
+
 const Trending = () => {
-  return <div>Trending</div>;
+  const { response } = useAxios("search/trending");
+
+  return (
+    <div className="mt-8">
+      <h1 className="text-2xl mb-2">Trending</h1>
+      {response &&
+        response.coins.map((coin) => (
+          <CoinTrending key={coin.item.coin_id} coin={coin.item} />
+        ))}
+    </div>
+  );
 };
 
 export default Trending;
