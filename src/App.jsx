edb7104637e303b31da1eb759efa3dccf132ce5d
sirdashasan <sirdashasan@hotmail.com>
2024-04-01 19:52:29 +0300

    HistoryChart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import CurrencyList from "./components/CurrencyList";
 import CryptoHome from "./pages/CryptoHome";
 import CryptoDetail from "./pages/CryptoDetail";
 import Navbar from "./components/Navbar";
